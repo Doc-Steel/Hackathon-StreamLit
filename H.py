8c332a1a263a0cdd1b6d7b926a75b5f148f59a12
Doc-Steel <93289194+Doc-Steel@users.noreply.github.com>
2024-11-16 16:58:00 +0000

    Add files via upload
    - Trying to make it so the correct country isnt always on the left side
    - Add images of chosen country

--- H.py
+++ H.py
@@ -1,40 +1,58 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import pycountry
+import random
+
+score = 0
+playerName = ""
+playerCorrect = False
+
+def correct_button():
+    st.session_state.clicked = True
+    st.session_state.score += 1
+    st.session_state.playerCorrect = True
+
+def wrong_button():
+    st.session_state.clicked = True
+    st.session_state.playerCorrect = False
+
+# Generate a list of country names
+country_names = [country.name for country in pycountry.countries]
+
+# Choose a random country
+chosen_country = random.choice(country_names)
+
+# Choose three other random names for buttons, excluding the chosen country
+other_countries = random.sample([name for name in country_names if name != chosen_country], 3)
+
 
 st.title("Mass RPG")
-x = st.button('Wearing Hat')  # 👈 this is a widget
-st.write('Wearing Hat? :', x)
 
 st.text_input("Enter Your Name", key="name")
 st.write('Your name is ', st.session_state.name)
 
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+if 'playerCorrect' not in st.session_state:
+    st.session_state.playerCorrect = False
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+if 'score' not in st.session_state:
+    st.session_state.score = 0
 
-    chart_data
+column1, column2, column3, column4  = st.columns(4)
+# You can use a column just like st.sidebar:
+column1.button(chosen_country, on_click=correct_button)
+column2.button(other_countries[0], on_click=wrong_button)
+column3.button(other_countries[1], on_click=wrong_button)
+column4.button(other_countries[2], on_click=wrong_button)
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
-
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected: ', option
-
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
-
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
+with st.empty():
+    if st.session_state.clicked:
+        if st.session_state.playerCorrect == True:
+            st.write("Correct!!!")
+            pass
+        else:
+            st.write("WRONG!!!")
+            pass
+    else:    
+        pass
